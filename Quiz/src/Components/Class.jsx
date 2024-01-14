686f1277192ce3b738233b6203ade482da10834c
EN-NAQQACH <mohssineennaqqach0@gmail.com>
2024-01-14 20:59:36 +0100

    Class

---
+++ Quiz/src/Components/Class.jsx
@@ -0,0 +1,15 @@
+
+function Class(){
+
+    return(
+        <div className="Content-Class">
+            <h2> Add new Class</h2>
+            <form action="">
+            <input type="text" placeholder='Class name' required={true} />
+            <button type="submit">Add</button>
+            </form>
+        </div>
+    )
+    
+}
+export default Class;
