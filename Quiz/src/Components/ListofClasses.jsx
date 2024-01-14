d41e0fbc8168e5a06a30f2676b65a9d1cabe001b
EN-NAQQACH <mohssineennaqqach0@gmail.com>
2024-01-14 23:45:05 +0100

    listofclasses

---
+++ Quiz/src/Components/ListofClasses.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import "../App.css"
+function ListofClasses() {
+    const [classes , setClasses] = useState([]);
+    const fetchClasses = async () => {
+        const response = await fetch("https://quiz-app.eroslabs.live/api/classes",{
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            }
+        });
+        const data = await response.json();
+        console.log(data);
+        const classesArray = data.hasOwnProperty('classes') ? data.classes : [];
+        setClasses(classesArray);
+        
+    }
+
+
+    useEffect(() => {
+        fetchClasses();
+    },[])
+
+  return (
+    <div className="Content-Class">
+      <h2>List of Classes</h2>
+      <table className="class-table">
+        <thead>
+          <tr>
+            <th>Class Id</th>
+            <th>Class</th> 
+          </tr>
+        </thead>
+        <tbody>
+        {Array.isArray(classes) ? (
+            classes.map((classInfo) => (
+              <tr key={classInfo._id}>
+                <td>{classInfo._id}</td>
+                <td>{classInfo.class_name}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="2">No classes found</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default ListofClasses;
