a91b91aeefd70036e20d6151f7d62f9b315b0fdf
EN-NAQQACH <mohssineennaqqach0@gmail.com>
2024-01-19 00:46:49 +0100

    Quizes in Class

---
+++ Quiz/src/Components/ListofClassesQ.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import { Link,useNavigate } from 'react-router-dom';
+import "../Styles/App.css"
+
+function ListofClassesQ() {
+  const [classes, setClasses] = useState([]);
+  const navigate = useNavigate();
+  const fetchClasses = async () => {
+    const response = await fetch("https://quiz-app.eroslabs.live/api/classes", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        'Authorization': `Bearer ${localStorage.getItem('token')}`
+      }
+    });
+    const data = await response.json();
+    console.log(data);
+    const classesArray = data.hasOwnProperty('classes') ? data.classes : [];
+    setClasses(classesArray);
+
+  }
+  useEffect(() => {
+      if (!localStorage.getItem('token')) {
+        navigate('/Login');
+      }
+    fetchClasses();
+  }, [])
+
+  return (
+    <div className="content">
+      <h2>List of Classes</h2>
+      <label htmlFor="">* Click on Quiz to display all Quizez in that class</label>
+      <table className="class-table">
+        <thead>
+          <tr>
+            <th>Class</th>
+            <th>Info</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Array.isArray(classes) ? (
+            classes.map((classInfo) => (
+              <tr key={classInfo.classObject._id}>
+                {/* <td><p>{classInfo.classObject._id}</p></td> */}
+                <td>{classInfo.classObject.class_name}</td>
+                <td><Link to={`/Quiz/${classInfo.classObject._id}`}>Quiz...</Link></td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="2">No classes found</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default ListofClassesQ;
