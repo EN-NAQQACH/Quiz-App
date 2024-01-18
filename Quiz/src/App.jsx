836d1cb5bf18101ad62f4c28b8008a18fa0d5441
EN-NAQQACH <mohssineennaqqach0@gmail.com>
2024-01-19 00:48:30 +0100

    Quiz Route

--- Quiz/src/App.jsx
+++ Quiz/src/App.jsx
@@ -1,6 +1,4 @@
-import { useState, useEffect} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import Signup from './Components/Signup';
 import Login from './Components/LogIn';
 import Home from './Components/HomePage';
@@ -12,7 +10,15 @@ import ListofStudentsinClass from './Components/ListofStudentsinClass';
 import JoinClass from './Components/JoinClass';
 import AddQuiz from './Components/AddQuiz';
 import Quiz from './Components/Quiz';
-
+import UserInfo from './Components/UserInfo';
+import ListofClassesQ from './Components/ListofClassesQ';
+import ListofQuizez from './Components/ListofQuizez';
+import UpdateQuiz from './Components/UpdateQuiz';
+import QuizForm from './Components/QuizForm';
+import QuizPage from './Components/QuizPage';
+import ScoreForm from './Components/ScoreForm';
+import ListStudentsinQuiz from './Components/ListStudentsInQuiz';
+import Page404 from './Components/Page404';
 
 function App() {
   const navigate = useNavigate();
@@ -34,17 +40,35 @@ function App() {
 
   return (
     <>
-     <Routes>
+      <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/info" element={<UserInfo />} />
         <Route path="/Signup" element={<Signup />} />
         <Route path="/Login" element={<Login setIsTeacher={setIsTeacher} />} />
-        <Route path="/class/Add" element={<Class />} />
-        <Route path="/classes" element={<ListofClasses />} />
-        <Route path="/classes/:classId/students" element={<ListofStudentsinClass />} />
-        <Route path="/class/Join" element={<JoinClass />} />
-        <Route path="/Quiz/Add" element={<AddQuiz />} />
+        <Route path="/Class/Add" element={<Class />} />
+        <Route path="/Classes" element={<ListofClasses />} />
+        <Route path="/Classes/:classId/students" element={<ListofStudentsinClass />} />
+        <Route path="/Class/Join" element={<JoinClass />} />
+        <Route path="/Quizez" element={<ListofClassesQ />} />
+        {/* Nested Quiz Routes */}
+        <Route path="/Quiz">
+          <Route path="Add" element={<AddQuiz />} />
+          <Route path="Add/:classId" element={<Quiz />} />
+          <Route path=":classId" element={<ListofQuizez isTeacher={isTeacher} />} />
+          <Route path=":classId/update/:quizId" element={<UpdateQuiz />} />
+          <Route path=":classId/start/:quizId" element={<QuizPage />} />
+          <Route path=":classId/result/:quizId/student/:studentId" element={<ScoreForm />} />
+          <Route path=":classId/ListStudents/:quizId" element={<ListStudentsinQuiz />} />
+        </Route>
+        {/* <Route path="/Quiz/Add" element={<AddQuiz />} />
         <Route path="/Quiz/Add/:classId" element={<Quiz />} />
-        <Route path="/Quizez" element={<ListofClasses />} />
+        <Route path="/Quizez" element={<ListofClassesQ />} />
+        <Route path="/Quiz/:classId" element={<ListofQuizez isTeacher={isTeacher} />} />
+        <Route path="/Quiz/:classId/update/:quizId" element={<UpdateQuiz />} />
+        <Route path="/Quiz/:classId/start/:quizId" element={<QuizPage />} />
+        <Route path="/Quiz/:classId/result/:quizId/student/:studentId" element={<ScoreForm />} />
+        <Route path="/Quiz/:classId/ListStudents/:quizId" element={<ListStudentsinQuiz />} /> */}
+        <Route path="*" element={<Page404 />} />
       </Routes>
       {location.pathname !== '/Signup' && location.pathname !== '/Login' && <Navbar isTeacher={isTeacher} />}
     </>
