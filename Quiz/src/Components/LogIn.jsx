22232a67fa1cfdaf8063014a8d67bc8a3e2a5ceb
EN-NAQQACH <mohssineennaqqach0@gmail.com>
2024-01-19 20:04:19 +0100

    Login / Sign up update

--- Quiz/src/Components/LogIn.jsx
+++ Quiz/src/Components/LogIn.jsx
@@ -5,9 +5,20 @@ import { Link, useNavigate } from 'react-router-dom';
 function LogIn({ setIsTeacher }) {
   const [token, setToken] = useState('');
   const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
   const [isTeacher, setIsTeacherLocal] = useState(false);
   const navigate = useNavigate();
+  const [password, setPassword] = useState('');
+
+
+  const handleFocus = (e) => {
+    e.target.parentNode.classList.add('focused');
+  };
+
+  const handleBlur = (e) => {
+    if (e.target.value === '') {
+      e.target.parentNode.classList.remove('focused');
+    }
+  };
   const apiurl = isTeacher
     ? 'https://quiz-app.eroslabs.live/api/user/teacher/login'
     : 'https://quiz-app.eroslabs.live/api/user/student/login';
@@ -54,7 +65,7 @@ function LogIn({ setIsTeacher }) {
       
           fetchUserInfo(); 
           alert('Login successful!');
-          navigate('/');
+          window.location.href = '/';
         } else {
           alert('Login failed');
         }
@@ -67,20 +78,81 @@ function LogIn({ setIsTeacher }) {
   }, []);
 
   return (
-    <div className="Login">
-      <h2>Log In</h2>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
-        <label>
-          <input type="checkbox" checked={isTeacher} onChange={() => setIsTeacherLocal(!isTeacher)} />
-          Login as Teacher
-        </label>
-        <button type="submit">Log In</button>
-        <p>
-          Don't have an account? <Link to="/Signup">Sign Up</Link>
-        </p>
+    // <div>
+    // <div className="Login">
+    //   <h2>Log In</h2>
+    //   <form onSubmit={handleSubmit}>
+    //     <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
+    //     <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+    //     <label>
+    //       <input type="checkbox" checked={isTeacher} onChange={() => setIsTeacherLocal(!isTeacher)} />
+    //       Login as Teacher
+    //     </label>
+    //     <button type="submit">Log In</button>
+    //     <p>
+    //       Don't have an account? <Link to="/Signup">Sign Up</Link>
+    //     </p>
+    //   </form>
+    // </div>
+    // </div>
+<div className="login-signup">
+<header>
+      <div>Quiz</div>
+      <div className="btn-login-signup">
+        <Link to="/Login"><button>Login</button></Link>
+        <Link to="/Signup"><button>Sign up</button></Link>
+      </div>
+      </header>
+    <div className="google-login-container">
+      
+      <h1>Welcome</h1>
+      <form className="google-login-form" onSubmit={handleSubmit}>
+        <fieldset className="google-login-fieldset">
+          <legend className="fieldset-legend">Account Login</legend>
+
+          <div className="google-login-field">
+            <label htmlFor="username" className="placeholder">username</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={username}
+              onChange={(e) => {setUsername(e.target.value)}}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+              required
+              
+            />
+          </div>
+
+          <div className="google-login-field">
+            
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="password"
+              value={password}
+              onChange={(e) => {setPassword(e.target.value)}}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+              required
+              
+              
+            />
+          </div>
+          <div className="google-login-checkbox">
+            <input
+              type="checkbox"
+              checked={isTeacher} onChange={() => setIsTeacherLocal(!isTeacher)}
+            />
+            <label htmlFor="">Teacher</label>
+          </div>
+          <button type="submit" id="signup">Sign in</button>
+        </fieldset>
       </form>
+      <p className="google-forgot-password">Don't have an account <Link to="/Signup">Sign up</Link></p>
+    </div>
     </div>
   );
 }
--- Quiz/src/Components/Signup.jsx
+++ Quiz/src/Components/Signup.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import React from 'react';
-import '../Styles/Signup.css';
+import '../Styles/Login.css';
 import { Link, useNavigate } from 'react-router-dom';
 
 function Signup() {
@@ -11,6 +11,17 @@ function Signup() {
     const [email, setemail] = useState('');
     const [password, setpassword] = useState('');
     const navigate = useNavigate();
+
+    const handleFocus = (e) => {
+        e.target.parentNode.classList.add('focused');
+      };
+    
+      const handleBlur = (e) => {
+        if (e.target.value === '') {
+          e.target.parentNode.classList.remove('focused');
+        }
+      };
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         const selectedRole = isStudent ? 'student' : isTeacher ? 'teacher' : null;
@@ -57,26 +68,120 @@ function Signup() {
         }
     }
     return (
-        <div className='Signup'>
-            <h2>Sign Up</h2>
-            <form onSubmit={handleSubmit}>
-                <input type="text" value={username} onChange={(e) => setusername(e.target.value)} placeholder='Username' />
-                <input type="text" value={full_name} onChange={(e) => setfullname(e.target.value)} placeholder='fullname' />
-                <input type="email" value={email} onChange={(e) => setemail(e.target.value)} placeholder='email@gmail.com' />
-                <input type="password" value={password} onChange={(e) => setpassword(e.target.value)} placeholder='password' />
-                <label htmlFor="">
-                    <label className='Student'>
-                        <input type="checkbox" name="student" checked={isStudent} onChange={handleChange} />
-                        Student
-                    </label>
-                    <label className='Teacher'>
-                        <input type="checkbox" name="teacher" checked={isTeacher} onChange={handleChange} />
-                        Teacher
-                    </label>
-                </label>
-                <button type="submit">Sign up</button>
-                <p>you have already an account ? <Link to="/Login">Log In</Link></p>
-            </form>
+        // <div className='Signup'>
+        //     <h2>Sign Up</h2>
+        //     <form onSubmit={handleSubmit}>
+        //         <input type="text" value={username} onChange={(e) => setusername(e.target.value)} placeholder='Username' />
+        //         <input type="text" value={full_name} onChange={(e) => setfullname(e.target.value)} placeholder='fullname' />
+        //         <input type="email" value={email} onChange={(e) => setemail(e.target.value)} placeholder='email@gmail.com' />
+        //         <input type="password" value={password} onChange={(e) => setpassword(e.target.value)} placeholder='password' />
+        //         <label htmlFor="">
+        //             <label className='Student'>
+        //                 <input type="checkbox" name="student" checked={isStudent} onChange={handleChange} />
+        //                 Student
+        //             </label>
+        //             <label className='Teacher'>
+        //                 <input type="checkbox" name="teacher" checked={isTeacher} onChange={handleChange} />
+        //                 Teacher
+        //             </label>
+        //         </label>
+        //         <button type="submit">Sign up</button>
+        //         <p>you have already an account ? <Link to="/Login">Log In</Link></p>
+        //     </form>
+        // </div>
+
+
+
+        <div className="login-signup">
+            <header>
+                <div>Quiz</div>
+                <div className="btn-login-signup">
+                <Link to="/Login"><button>Login</button></Link>
+        <Link to="/Signup"><button>Sign up</button></Link>
+                </div>
+            </header>
+            <div className="google-login-container">
+                <h1>Welcome</h1>
+                <form className="google-login-form" onSubmit={handleSubmit}>
+                    <fieldset className="google-login-fieldset">
+                        <legend className="fieldset-legend">Account Sign Up</legend>
+
+                        <div className="google-login-field">
+                            <label htmlFor="username" className="placeholder">username</label>
+                            <input
+                                type="text"
+                                id="username"
+                                name="username"
+                                value={username}
+                                onChange={(e) => { setusername(e.target.value) }}
+                                onFocus={handleFocus}
+                                onBlur={handleBlur}
+                                required
+
+                            />
+                        </div>
+                        <div className="google-login-field">
+                            
+                            <input
+                                type="text"
+                                id="full_name"
+                                placeholder='fullname'
+                                name="username"
+                                value={full_name}
+                                onChange={(e) => { setfullname(e.target.value) }}
+                                required
+
+                            />
+                        </div>
+                        <div className="google-login-field">
+                            
+                            <input
+                                type="email"
+                                id="email"
+                                placeholder='email'
+                                name="username"
+                                value={email}
+                                onChange={(e) => { setemail(e.target.value) }}
+                                required
+
+                            />
+                        </div>
+
+                        <div className="google-login-field">
+
+                            <input
+                                type="password"
+                                id="password"
+                                name="password"
+                                placeholder="password"
+                                value={password}
+                                onChange={(e) => { setpassword(e.target.value) }}
+                                required
+
+
+                            />
+                        </div>
+                        <div className="google-login-checkbox">
+                            <input
+                                type="checkbox"
+                                name="teacher"
+                                checked={isTeacher} onChange={handleChange}
+                            />
+                            <label htmlFor="">Teacher</label>
+                        </div>
+                        <div className="google-login-checkbox">
+                            <input
+                                type="checkbox"
+                                name="student"
+                                checked={isStudent} onChange={handleChange}
+                            />
+                            <label htmlFor="">Student</label>
+                        </div>
+                        <button type="submit" id="signup">Sign up</button>
+                    </fieldset>
+                </form>
+                <p className="google-forgot-password">you already have an account <Link to="/Login">Log in</Link></p>
+            </div>
         </div>
     )
 }
